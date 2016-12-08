81f7000509482aff8afc57e260f0cc83f5c20ce2
Mustafa Yasin HALICI <mustafayasinhalici@gmail.com>
2016-12-08 15:43:40 +0300

    uploader interface and uploader server

---
+++ server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"net/http"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func upload(c echo.Context) error {
+
+	file, err := c.FormFile("file")
+	if err != nil {
+		return err
+	}
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	dst, err := os.Create(file.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.HTML(http.StatusOK, fmt.Sprintf("<p>File %s uploaded successfully.</p>", file.Filename))
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Static("/", "public")
+	e.POST("/upload", upload)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
